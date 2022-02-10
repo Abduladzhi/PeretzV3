212eabf8f996e9541110d64ebdf27391851df652
Abduladzhi <Abduladzhi@mail.ru>
2022-02-10 20:03:34 +0300

    FirstGit

---
+++ PeretzV3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeretzV3
+//
+//  Created by Abduladzhi on 10.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+
