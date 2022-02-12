e8ccc92e1103081382bd8ac257a485fff076ca81
Abduladzhi <Abduladzhi@mail.ru>
2022-02-12 04:17:07 +0300

    first ready project

--- PeretzV3/Menu/View/MenuCell.swift
+++ PeretzV3/Menu/View/MenuCell.swift
@@ -17,33 +17,14 @@ class MenuCell: UITableViewCell{
     @IBOutlet weak var moreButton: UIButton!
     @IBOutlet weak var lessButton: UIButton!
     @IBOutlet weak var bigImage: UIImageView!
-    
+
     func configure(with menu: Menu) {
         self.nameLabel.text = menu.name
         self.descriptionLabel.text = menu.description
         self.priceLabel.text = String ("\(menu.price)₽")
         self.lessButton.layer.cornerRadius = lessButton.frame.height/2
         self.moreButton.layer.cornerRadius = moreButton.frame.height/2
-        
-        if let image = getImage(from: menu.image) {
-            self.bigImage.image = image
-        }
+        self.bigImage.downloaded(from: menu.image)
     }
 }
-    func getImage(from string: String) -> UIImage? {
-        guard let url = URL(string: string)
-            else {
-                print("Unable to create URL")
-                return nil
-        }
-        var image: UIImage? = nil
-        do {
-            let data = try Data(contentsOf: url, options: [])
-            image = UIImage(data: data)
-        }
-        catch {
-            print(error.localizedDescription)
-        }
-        return image
-    }
 
