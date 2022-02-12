fe359966186ef683faca1548b77b48c0975083f7
Abduladzhi <Abduladzhi@mail.ru>
2022-02-12 14:31:38 +0300

    second ready project

---
+++ PeretzV3/Application/View/First sceneViewController.swift
@@ -0,0 +1,22 @@
+//
+//  First sceneViewController.swift
+//  PeretzV3
+//
+//  Created by Abduladzhi on 12.02.2022.
+//
+
+import UIKit
+
+class First_sceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+    }
+    func setupNavigationBar() {
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.tintColor = .white
+//        navigationController?.navigationBar.
+//        navigationItem.largeTitleDisplayMode = .always
+    }
+}
